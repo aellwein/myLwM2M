379fa6866083429edc0fa4ad688355996856e72b
Alex Ellwein <alex.ellwein@gmail.com>
2015-04-01 22:32:03 +0200

    enable test command for setup.py/setuptools

--- setup.py
+++ setup.py
@@ -1,7 +1,30 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import sys
 from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
 
 setup(
     name="myLwM2M",
@@ -11,5 +34,6 @@ setup(
     author_email="mylwm2m@ellwein.net",
     license="MIT License",
     install_requires=["aiocoap"],
-    tests_require=["pytest"]
+    tests_require=["pytest"],
+    cmdclass={'test': PyTest}
 )
